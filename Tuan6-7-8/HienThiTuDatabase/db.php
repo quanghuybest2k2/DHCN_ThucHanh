218de2d9c7a07a1ae9c4db74bdb16e6cb1d413e7
Đoàn Quang Huy <82820145+quanghuybest2k2@users.noreply.github.com>
2022-10-10 14:07:40 +0700

    Update db.php

--- Tuan6-7-8/HienThiTuDatabase/db.php
+++ Tuan6-7-8/HienThiTuDatabase/db.php
@@ -1,9 +1,11 @@
 <?php
+header("Content-type: text/html; charset=utf-8");
 $server = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = 'ql_ban_sua';
 $conn = mysqli_connect($server, $username, $password, $dbname);
+mysqli_set_charset($conn, 'UTF8');
 if ($conn->connect_error) {
     die("Kết nối db không thành công: " . $conn->connect_error);
 }
